f8cb2cc10d8d34d5820d0dccfed5cd08be58aea7
Leo Dasso <leo.dasso@gmail.com>
2019-05-16 13:35:29 +0900

    add conclusion of drawing and path exploding

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,38 +1,72 @@
 import React, { Component } from "react";
-import { domToCanvasCoords} from "../calc";
+import { domToCanvasCoords, randomRange, lerp} from "../calc";
 
 class Canvas extends Component {
-
 
 	state = {
 		mouseX: 0,
 		mouseY: 0,
 		pathPoints: [],
 		age: 0,						// in milliseconds
+		duration: 5000,
+		timerIntervalId: -99,
 	}
-
 
 	componentDidMount() 
 	{
-
 		//add a counter 
-		setInterval(() => {
-			this.setState({age: this.state.age + 20});
-		}, 20)
-
+		const timerInterval = setInterval(this.increment, 20)
+		this.setState({timerIntervalId: timerInterval});
 
 		this.initCanvas();
 		// When the mouse moves, we want to update the state with it's new position
 		window.addEventListener( "mousemove",
 			event => {
+
+				if (!this.isDrawingTime()) return;
+
 				this.setState({
 					mouseX: event.screenX, 
 					mouseY: event.screenY,
-					pathPoints: [...this.state.pathPoints, {x: event.screenX, y: event.screenY, t: this.state.age}],
+					pathPoints: [...this.state.pathPoints, 
+						{
+							x: event.screenX, 
+							y: event.screenY, 
+							// Velocity
+							vx: 0,
+							vy: 0,
+							gravity: 0,
+							drag: .02,
+							opacity: 100,
+							// This is the opacity that gets lerped to
+							finalOpacity: 100,
+							t: this.state.age}],
 				});
 			},
 			false
 		);
+	}
+
+
+	// Increment the age of the component, and checks if drawing time is complete
+	increment = () => {
+		
+		this.setState({age: this.state.age + 20});
+
+		if (this.state.age > this.state.duration) {
+			clearInterval(this.state.timerIntervalId);
+			this.exlpodePath();
+		}
+	}
+
+	exlpodePath() {
+
+		for (let pt of this.state.pathPoints) {
+			pt.vx = randomRange(-8, 8);
+			pt.vy = randomRange(-8, 8);
+			pt.gravity = .1;
+			pt.finalOpacity = 0;
+		}
 	}
 
 
@@ -44,6 +78,8 @@ class Canvas extends Component {
 		
 		setInterval(this.updateCanvas, 30);
 	}
+
+	isDrawingTime = () => this.state.age < this.state.duration;
 
 
 	updateCanvas = () => 
@@ -60,29 +96,56 @@ class Canvas extends Component {
 		ctx.beginPath();
 		ctx.arc(canvasCoords.x, canvasCoords.y - 80, 10, 0, 2 * Math.PI);
 		ctx.fill();
+		ctx.closePath();
 
-		// render the full path
-		for (let pt of this.state.pathPoints) {
+		// render each point in the path
+		for (let i = 1; i < this.state.pathPoints.length; i++) {
 
+			const pt = this.state.pathPoints[i];
+			const prevPt = this.state.pathPoints[i-1];
 			const lifetime = this.state.age - pt.t;
-			if (lifetime > 8000) continue;
+			if (lifetime > this.state.duration) continue;
 
-			const ratio = lifetime / 8000;
-			ctx.fillStyle = `hsl(100, 50%, ${ratio * 100}%)`;
+			pt.opacity = lerp(pt.opacity, pt.finalOpacity, .05);
+
+			const ratio = lifetime / this.state.duration;
+			ctx.strokeStyle = `hsla(100, 50%, ${ratio * 100}%, ${pt.opacity}%)`;
+			ctx.lineWidth = 6 * (1 - ratio);
+
 			const canvasCoords = domToCanvasCoords(canvas, {x:pt.x, y:pt.y});
+			const prevCanvasCoords = domToCanvasCoords(canvas, {x:prevPt.x, y:prevPt.y});
+
+			// apply gravity to the point
+			pt.vy += pt.gravity;
+			// update the position of the point
+			pt.x += pt.vx;
+			pt.y += pt.vy;
+			// apply drag to the point
+			pt.vx -= pt.vx * pt.drag;
+			pt.vy -= pt.vy * pt.drag;
+
 			ctx.beginPath();
-			ctx.arc(canvasCoords.x, canvasCoords.y - 80, 2, 0, 2 * Math.PI);
-			ctx.fill();	
-			
+			ctx.moveTo(prevCanvasCoords.x, prevCanvasCoords.y - 80);
+			ctx.lineTo(canvasCoords.x, canvasCoords.y - 80);
+			ctx.stroke();
+			ctx.closePath();
 		};
+		
 	}
 
 
 	render() {
 
+		// get the remaining time in seconds
+		let remainingTime = (this.state.duration - this.state.age) / 1000;
+		// make it read as a clean 2 decimal number
+		remainingTime = remainingTime.toFixed(2);
+		// clamp to 0
+		if (remainingTime < 0) remainingTime = 0;
+
 		return (
 			<div>
-				<p>{this.state.age}</p>
+				<p>Draw for {remainingTime} seconds!</p>
 				<canvas ref="canvas" className="canvas" />
 			</div>
 		);
